796a6779be0265080113a2d5bc4927579c88e9f8
W1ndys <w1ndys@outlook.com>
2024-12-03 23:24:05 +0800

    Refactor add_text_to_image function to support platform-specific font paths and color customization; change async functions to regular functions for better compatibility. Add new add_text_welcome_party function for generating welcome party images. Remove unused yuanloo.png file.

--- main.py
+++ main.py
@@ -45,9 +45,19 @@ def save_function_status(group_id, status):
 
 
 # 通用的添加文字到图片的函数
-async def add_text_to_image(input_image, box, text, angle=0):
-    font_path = "/usr/share/fonts/truetype/win/MSYH.TTC"
-    initial_font_size = 10
+def add_text_to_image(
+    input_image,
+    box,
+    text,
+    angle=0,
+    color=(0, 0, 0, 255),
+    winfont_path="C:/Windows/Fonts/SIMHEI.TTF",
+    linuxfont_path="/usr/share/fonts/truetype/win/SIMHEI.TTF",
+):
+
+    font_path = winfont_path if os.name == "nt" else linuxfont_path
+
+    # initial_font_size = 10
     img = Image.open(input_image)
     draw = ImageDraw.Draw(img)
 
@@ -55,7 +65,7 @@ async def add_text_to_image(input_image, box, text, angle=0):
     box_width, box_height = box[2] - box[0], box[3] - box[1]
     box_x, box_y = box[0], box[1]
 
-    font_size = initial_font_size
+    font_size = 15
     text_width, text_height = 0, 0
     font = ImageFont.truetype(font_path, font_size)
 
@@ -80,7 +90,7 @@ async def add_text_to_image(input_image, box, text, angle=0):
     text_layer = Image.new("RGBA", img.size, (255, 255, 255, 0))
     text_draw = ImageDraw.Draw(text_layer)
     font = ImageFont.truetype(font_path, font_size)
-    text_draw.text((x, y), text, font=font, fill=(0, 0, 0, 255))
+    text_draw.text((x, y), text, font=font, fill=color)
 
     # 旋转文本图层
     rotated_text_layer = text_layer.rotate(
@@ -101,24 +111,31 @@ async def add_text_to_image(input_image, box, text, angle=0):
 
 
 # 狂粉
-async def add_text_kf(text="W1ndys"):
+def add_text_kf(text="W1ndys"):
     img_path = os.path.join(INPUT_DIR, "love.png")
     box = (257, 21, 724, 252)
-    return await add_text_to_image(img_path, box, text, angle=-5)
+    return add_text_to_image(img_path, box, text, angle=-5)
 
 
 # 向上竖起大拇指
-async def add_text_up_hand(text="W1ndys"):
+def add_text_up_hand(text="W1ndys"):
     img_path = os.path.join(INPUT_DIR, "up_hand.png")
     box = (30, 690, 931, 907)
-    return await add_text_to_image(img_path, box, text, angle=0)
+    return add_text_to_image(img_path, box, text, angle=0)
 
 
-# 源鲁杯邀请函
-async def add_text_invitation(text="Yuanloo"):
-    img_path = os.path.join(INPUT_DIR, "yuanloo.png")
-    box = (135, 713, 626, 819)
-    return await add_text_to_image(img_path, box, text, angle=0)
+# 网安迎新晚会
+def add_text_welcome_party(text="W1ndys"):
+    img_path = os.path.join(INPUT_DIR, "cyber.png")
+    box = (236, 473, 354, 527)
+    return add_text_to_image(
+        img_path,
+        box,
+        text,
+        angle=0,
+        color=(255, 255, 255, 255),
+        linuxfont_path="/usr/share/fonts/truetype/win/YanShiXiaXingKai-2.ttf",
+    )
 
 
 # 群消息处理函数
@@ -141,7 +158,7 @@ async def handle_ImageGenerate_group_message(websocket, msg):
                         websocket, group_id, "图片生成中..."
                     )
                     prompt = match.group(1)
-                    img_path = await add_text_kf(prompt)
+                    img_path = add_text_kf(prompt)
 
                     # 文件
                     if img_path:
@@ -163,7 +180,7 @@ async def handle_ImageGenerate_group_message(websocket, msg):
                         websocket, group_id, "图片生成中..."
                     )
                     prompt = match.group(1)
-                    img_path = await add_text_up_hand(prompt)
+                    img_path = add_text_up_hand(prompt)
 
                     # 文件
                     if img_path:
@@ -178,28 +195,44 @@ async def handle_ImageGenerate_group_message(websocket, msg):
                         f"[CQ:reply,id={message_id}]输入内容不合法，请重新输入",
                     )
 
-        elif raw_message.startswith("邀请函"):
-            match = re.search(r"邀请函(.*)", raw_message)
+        elif raw_message.startswith("网安邀请函"):
+            match = re.search(r"网安邀请函(.*)", raw_message)
             if match:
-                if len(match.group(1)) <= 199 and len(match.group(1)) > 0:
+                if len(match.group(1)) <= 6 and len(match.group(1)) > 0:
                     del_message_id = await send_group_msg_with_reply(
                         websocket, group_id, "图片生成中..."
                     )
                     prompt = match.group(1)
-                    img_path = await add_text_invitation(prompt)
-
-                    # 文件
-                    if img_path:
-                        await delete_msg(websocket, del_message_id)
-                        await send_group_msg(
-                            websocket, group_id, f"[CQ:image,file=file://{img_path}]"
-                        )
-                else:
+                    img_path = add_text_welcome_party(prompt)
+                elif len(match.group(1)) > 6:
                     await send_group_msg(
                         websocket,
                         group_id,
-                        f"[CQ:reply,id={message_id}]输入内容不合法，请重新输入",
+                        f"[CQ:reply,id={message_id}]太长了",
                     )
+                elif len(match.group(1)) == 0:
+                    await send_group_msg(
+                        websocket,
+                        group_id,
+                        f"[CQ:reply,id={message_id}]啥也没输你生成啥",
+                    )
+            else:
+                prompt = "W1ndys"
+                img_path = add_text_welcome_party(prompt)
+
+            # 文件
+            if img_path:
+                await delete_msg(websocket, del_message_id)
+                await send_group_msg(
+                    websocket, group_id, f"[CQ:image,file=file://{img_path}]"
+                )
+            else:
+                await send_group_msg(
+                    websocket,
+                    group_id,
+                    f"[CQ:reply,id={message_id}]输入内容不合法，请重新输入",
+                )
+
     except Exception as e:
         logging.error(f"处理ImageGenerate群消息失败: {e}")
         return
